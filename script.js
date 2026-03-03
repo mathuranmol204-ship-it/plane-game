255ca41e06c254add1f6d9eaf987f97d102fb912
mathuranmol204-ship-it <mathuranmol204@gmail.com>
2026-03-03 15:05:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ document.getElementById("startBtn").addEventListener("click", function() {
   gameStarted = true;
   this.style.display = "none";
 });
+let plane = document.getElementById("plane");
+let positionX = 100;
+let positionY = 200;
+let gameStarted = false;
+
+// Start button click
+document.getElementById("startBtn").addEventListener("click", function() {
+  gameStarted = true;
+  this.style.display = "none";
+});
+
+// Keyboard movement
+document.addEventListener("keydown", function(event) {
+
+  if (!gameStarted) return; // jab tak start nahi dabaya, kuch nahi hoga
+
+  if (event.key === "ArrowUp") {
+    positionY -= 10;
+  }
+  if (event.key === "ArrowDown") {
+    positionY += 10;
+  }
+  if (event.key === "ArrowRight") {
+    positionX += 10;
+  }
+  if (event.key === "ArrowLeft") {
+    positionX -= 10;
+  }
+
+  plane.style.left = positionX + "px";
+  plane.style.top = positionY + "px";
+});
